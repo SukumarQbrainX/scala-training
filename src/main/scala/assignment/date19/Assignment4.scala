867385c925ea59b0162149ff4077b7acab356c5d
Deepa-Tambad <86017554+Deepa-Tambad@users.noreply.github.com>
2021-06-22 13:04:06 +0530

    Update Assignment4.scala

--- src/main/scala/assignment/date19/Assignment4.scala
+++ src/main/scala/assignment/date19/Assignment4.scala
@@ -12,6 +12,36 @@ package assignment.date19
  *  in object `Main` write a function named "assignment4Run()"
  *   and write a logic to print the output on console.
  */
+import scala.Long;
+public class User {
+        private final String name;
+        private final long mobileNumber;
+    public User(String name, long mobileNumber) {
+            this.name = name;
+            this.mobileNumber = mobileNumber;
+        }
+        public String getName() {
+            return name;
+        }
+        public long getMobileNumber() {
+            return mobileNumber;
+        }
+    }
+
+package assignment.date19
 class Assignment4 {
+  def assignment4Run(name:String,mobileNumber:Long): Unit ={
+    val u=new User(name,mobileNumber)
+    println(u.getName+" --> "+u.getMobileNumber)
+  }
+  def main(args:Array[String]): Unit ={
+
+    assignment4Run("AAA",123456789)
+    assignment4Run("BBB",103050709)
+    assignment4Run("CCC",2040608010)
+    assignment4Run("DDD",123123123)
+  }
+}
+
 
 }
